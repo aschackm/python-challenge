d208aa82f0fc89d96a4009c4aec36beb73f8a5c7
aschackm <aschackm@gmail.com>
2018-05-04 21:50:56 -0500

    minor changes

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -25,12 +25,13 @@ with open(txtpath, 'r') as paragraph:
 ave_letter_count = sum(len(word) for word in word_list) / len(word_list)
 ave_sentence_length = sum(len(sentence) for sentence in sentence_list) / len(sentence_list)
 
+print("-"*40)
 print("Paragraph Analysis")
 print("-"*40)
 print("Approximate Word Count:" + str(word_count))
 print("Approximate Sentence Count:" + str(sentence_count))
 print("Average Letter Count:" + str(ave_letter_count))
 print("Average Sentence Length:" + str(ave_sentence_length))
-
+print("-"*40)
     
 
